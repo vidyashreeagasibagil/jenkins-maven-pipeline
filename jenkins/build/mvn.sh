c4a9c18626d54ad712839f3fb984e1e04b3b8bb4
vidya <vidya@localhost.localdomain>
2022-01-24 08:11:39 -0500

    added

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,6 @@ echo "*******************"
 echo "*****Building jar**"
 echo "*******************"
 
+WORKSPACE=/home/vidya/jenkins/jenkins_home/workspace/maven-jenkins-pipeline
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.2-ibmjava-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.2-ibmjava-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,6 @@ echo "*******************"
 echo "*****Testing the code**"
 echo "*******************"
 
+WORKSPACE=/home/vidya/jenkins/jenkins_home/workspace/maven-jenkins-pipeline
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.2-ibmjava-alpine mvn test
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.2-ibmjava-alpine mvn test
