5df8aa8fa2fb493d0946ef5edfdc300c3e96947c
vidya <vidya@localhost.localdomain>
2022-01-24 07:41:39 -0500

    added

---
+++ jenkins/deploy/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo maven-project > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
+
+scp /tmp/.auth prod-user@192.168.43.247:/tmp/
+scp ./jenkins/deploy/publish prod-user@192.168.43.247:/tmp/publish
