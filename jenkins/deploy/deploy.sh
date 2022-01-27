506bcfd19e3032beecbe3b0d9868982c0462d1d9
vidyashreeagasibagil <64072083+vidyashreeagasibagil@users.noreply.github.com>
2022-01-27 23:43:19 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -9,7 +9,4 @@ echo $PASS >> /tmp/.auth
 
 scp /tmp/.auth ec2-user@184.72.65.174:/tmp/
 scp ./jenkins/deploy/publish ec2-user@184.72.65.174:/tmp/publish
-
-sshagent(['184.72.65.174']) {
-    sh 'ssh -o StrictHostKeyChecking=no ec2-user@184.72.65.174 "/tmp/publish"'
-    }
+ssh -o StrictHostKeyChecking=no ec2-user@184.72.65.174 "/tmp/publish"
