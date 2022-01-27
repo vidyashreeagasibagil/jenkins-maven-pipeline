6b58b00ab8999c323f12ce55aba8a314900b863b
vidyashreeagasibagil <64072083+vidyashreeagasibagil@users.noreply.github.com>
2022-01-27 23:22:09 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp /tmp/.auth prod-user@192.168.43.247:/tmp/
-scp ./jenkins/deploy/publish prod-user@192.168.43.247:/tmp/publish
-ssh -o StrictHostKeyChecking=no prod-user@192.168.43.247 "/tmp/publish"
+scp /tmp/.auth ec2-user@184.72.65.174:/tmp/
+scp ./jenkins/deploy/publish ec2-user@184.72.65.174:/tmp/publish
+ssh -o StrictHostKeyChecking=no ec2-user@184.72.65.174 "/tmp/publish"
