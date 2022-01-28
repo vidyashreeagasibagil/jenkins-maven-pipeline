287ca3daee89cef6b79e326454fe9da9eea06ede
vidyashreeagasibagil <64072083+vidyashreeagasibagil@users.noreply.github.com>
2022-01-28 13:48:03 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,6 +7,6 @@ echo $PASS >> /tmp/.auth
 
 
 
-scp -i /opt/prod /tmp/.auth prod-user@192.168.43.247:/tmp/
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@192.168.43.247:/tmp/publish
-ssh -i /opt/prod prod-user@192.168.43.247 "/tmp/publish"
+scp /tmp/.auth prod-user@192.168.43.247:/tmp/
+scp ./jenkins/deploy/publish prod-user@192.168.43.247:/tmp/publish
+ssh -o StrictHostKeyChecking=no prod-user@192.168.43.247 "/tmp/publish"
